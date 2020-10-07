95908bb80cd436f7f0c315c4c52e5664ec4f0da6
Jola-Witaszak <jola-w@o2.pl>
2020-10-07 23:43:56 +0200

    Task 1 - new branch.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -2,6 +2,6 @@ package com.kodilla.testing;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.print("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        System.out.print("Moduł 6.1 Wprowadzenie do testowania oprogramowania");
     }
 }
