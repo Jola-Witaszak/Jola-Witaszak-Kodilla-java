aa92ea6d40bd3b38bda31501c2f0a160e2114936
Jola-Witaszak <jola-w@o2.pl>
2020-10-08 20:37:26 +0200

    6.2  -added module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,15 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.print("Moduł 6.1 Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator();
+        int additionResult = calculator.add(5,5);
+        int subtractResult = calculator.subtract(30, 10);
+            if (additionResult == 10 && subtractResult == 20) {
+                System.out.println("Calculator test is OK!");
+            } else {
+                System.out.println("Oops ... something went wrong :)");
+            }
     }
 }
