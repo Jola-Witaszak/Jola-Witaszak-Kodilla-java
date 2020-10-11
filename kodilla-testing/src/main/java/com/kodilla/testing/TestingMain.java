e7c160ea1636769bc53db93bc263d0f63e315eeb
Jola-Witaszak <jola-w@o2.pl>
2020-10-11 13:47:39 +0200

    Moduł 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,13 +3,5 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        int additionResult = calculator.add(5,5);
-        int subtractResult = calculator.subtract(30, 10);
-            if (additionResult == 10 && subtractResult == 20) {
-                System.out.println("Calculator test is OK!");
-            } else {
-                System.out.println("Oops ... something went wrong :)");
-            }
     }
 }
