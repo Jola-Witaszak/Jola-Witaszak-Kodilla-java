a404b7865a9d2fe842078d7c8a9d7e95c8121135
Jola-Witaszak <71170022+Jola-Witaszak@users.noreply.github.com>
2020-11-06 12:11:17 +0100

    module 7.5 - IntStream on books (#13)

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,19 @@
 package com.kodilla.stream;
 
 
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.person.People;
 
 public class StreamMain {
    public static void main(String[] args) {
 
        People.getList().stream()
-               .map(s -> s.toUpperCase())
+               .map(String::toUpperCase)
                .forEach(System.out :: println);
+
+       BookDirectory bookDirectory = new BookDirectory();
+       bookDirectory.getList().stream()
+               .filter(book -> book.getYearOfPublication() > 2005)
+               .forEach(System.out::println);
     }
 }
