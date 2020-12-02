fa9201f6541235cebc5166c5bd759f5c8b29f894
Jola-Witaszak <jola-w@o2.pl>
2020-12-02 17:42:46 +0100

    task 7.5 test fix

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -2,9 +2,6 @@ package com.kodilla.stream.array;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.OptionalDouble;
-import java.util.stream.IntStream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArrayOperationsTestSuite {
@@ -14,14 +11,9 @@ public class ArrayOperationsTestSuite {
         int[] numbers = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
 
         //When
-        ArrayOperations.getAverage(numbers);
+        double result = ArrayOperations.getAverage(numbers);
 
         //Then
-        double result = IntStream.range(0, numbers.length)
-                .map(i -> numbers[i])
-                .average()
-                .orElse(0);
-
         assertEquals(2.0, result, 0.001);
     }
 }
