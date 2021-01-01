4653650180ee55176aa9dd01e9ef9952d2b87bf8
Jola-Witaszak <71170022+Jola-Witaszak@users.noreply.github.com>
2021-01-01 15:07:04 +0100

    module-15.2 "Prototype - deepCopy(), shallowCopy()" (#32)

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/Board.java
@@ -0,0 +1,74 @@
+package com.kodilla.patterns.prototype;
+
+import javax.swing.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Board extends Prototype<Board> {
+    private String name;
+    private Set<TasksList> lists = new HashSet<>();
+
+    public Board(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<TasksList> getLists() {
+        return lists;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLists(Set<TasksList> lists) {
+        this.lists = lists;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Board [" + name + "]\n";
+        for (TasksList list : lists) {
+            s = s + list.toString() + "\n";
+        }
+        return s;
+    }
+
+    public Board shallowCopy() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Board deepCopy() throws CloneNotSupportedException {
+        Board clonedBoard = super.clone();
+        clonedBoard.lists = new HashSet<>();
+        for (TasksList theList : lists) {
+            TasksList clonedList = new TasksList(theList.getName());
+            for (Task task : theList.getTasks()) {
+                clonedList.getTasks().add(task);
+            }
+            clonedBoard.getLists().add(clonedList);
+        }
+        return clonedBoard;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Board board = (Board) o;
+
+        if (getName() != null ? !getName().equals(board.getName()) : board.getName() != null) return false;
+        return getLists() != null ? getLists().equals(board.getLists()) : board.getLists() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getLists() != null ? getLists().hashCode() : 0);
+        return result;
+    }
+}
